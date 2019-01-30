d220a7a7b1ac6ac53a575d6e2cf86f272a4352ff
Logan Wohlers <lawohlers@gmail.com>
2019-01-30 01:42:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  const z=document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for(let i=0;i<z.length; i++){
+    z[i].innerHTML=(parseInt(z[i].innerHTML)+n);
+  }
+  
+}
+
+function deepestChild (){
+ const x= document.querySelectorAll('#grand-node div');
+ return x[x.length-1];
+ 
+  
+  
+}
